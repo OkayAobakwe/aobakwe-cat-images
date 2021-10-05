c1ea746c1a596a5255c74cf0c4b9a8b31422aa19
aobakwe sometimes <okayaobakwe@gmail.com>
2021-10-05 15:27:53 +0200

    chore(custom-types): remove any for custom types

--- web/src/pages/cat-search/[id].tsx
+++ web/src/pages/cat-search/[id].tsx
@@ -6,9 +6,22 @@ import { StarIcon } from "@chakra-ui/icons"
 import axios from "axios"
 import { CAT_API, API_HEADERS } from "../../constants/index"
 
+export interface CatProps {
+ name: string;
+ id: string;
+ url: string
+}
+interface CatDescriptionProps {
+  breeds : [{
+    description: string
+  }];
+  categories: [{
+    name: string
+  }]
+}
 const CatSearch: FC = () => {
   const router = useRouter()
-  const [catImages, setCatImages] = useState<any>()
+  const [catImages, setCatImages] = useState<CatProps[]>()
   const [catDescription, setCatDescription] = useState("")
   const routerCheck = typeof router.query.id === "string" ? parseInt(router.query.id) : -1
   const [favouriteCats, setFavouriteCats] = useState([""])
@@ -24,41 +37,41 @@ const CatSearch: FC = () => {
     )
     .then((res) => {
       setCatImages(res?.data)
-      {!routerCheck ? setCatDescription(res?.data[0]?.breeds[0]?.description) :
-        setCatDescription(res?.data[0]?.categories[0]?.name)}
+      {!routerCheck ? setCatDescription((res?.data[0] as CatDescriptionProps)?.breeds[0]?.description) :
+        setCatDescription((res?.data[0] as CatDescriptionProps)?.categories[0]?.name)}
     })
     .catch((err) => {})
   }, [])
   
   return(
     <>
-    <NavBar/>
-    <Container centerContent>
-      {!routerCheck ? (
-        <>
-          <Heading>{router?.query?.id}</Heading>
-          <Text fontSize="md">{catDescription}</Text>
-        </>
-      )
-      : (
-        <Heading>{catDescription}</Heading>
-      )}
-      <Flex flexDirection="column">
-        {catImages?.map((catImage) => (
+      <NavBar/>
+      <Container centerContent>
+        {!routerCheck ? (
           <>
-            <Image key={catImage?.id} src={catImage?.url}/>
-            <IconButton
-              icon={<StarIcon />}
-              aria-label="favourite image"
-              onClick={() => {
-                setFavouriteCats([...favouriteCats, catImage?.url])
-                localStorage.setItem("favouriteCats", JSON.stringify(favouriteCats));
-              }}
-            />
+            <Heading>{router?.query?.id}</Heading>
+            <Text fontSize="md">{catDescription}</Text>
           </>
-        ))}
-      </Flex>
-    </Container>
+        )
+        : (
+          <Heading>{catDescription}</Heading>
+        )}
+        <Flex flexDirection="column">
+          {catImages?.map((catImage) => (
+            <>
+              <Image key={catImage?.id} src={catImage?.url}/>
+              <IconButton
+                icon={<StarIcon />}
+                aria-label="favourite image"
+                onClick={() => {
+                  setFavouriteCats([...favouriteCats, catImage?.url])
+                  localStorage.setItem("favouriteCats", JSON.stringify(favouriteCats));
+                }}
+              />
+            </>
+          ))}
+        </Flex>
+      </Container>
     </>
   )
 }
