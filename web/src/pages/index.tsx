146bc071e9b7b0a6ea72762c31cd87397cf114b1
aobakwe sometimes <okayaobakwe@gmail.com>
2021-10-05 13:09:21 +0200

    feat(index-page): change the index page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,28 +1,29 @@
 import {
   Link,
   Text,
-  
+  Flex
 } from '@chakra-ui/react'
 import NextLink from "next/link"
 import { Container } from '../components/Container'
-import { Search } from "../components/Search"
 import { NavBar } from "../components/Navbar"
+import React from 'react'
 
 const Index = () => (
   <>
     <NavBar />
     <Container height="100vh">
-      <NextLink href="/breeds/" as="/breeds/">
-        <Link>
-          <Text>Breeds</Text>
-        </Link>
-      </NextLink>
-      <NextLink href="/categories/" as="/categories/">
-        <Link>
-          <Text>Categories</Text>
-        </Link>
-      </NextLink>
-      <Search />
+      <Flex justifyContent="center" m="auto">
+        <NextLink href="/breeds/" as="/breeds/">
+          <Link marginRight={8}>
+            <Text fontSize="3xl">Breeds</Text>
+          </Link>
+        </NextLink>
+        <NextLink href="/categories/" as="/categories/">
+          <Link>
+            <Text fontSize="3xl">Categories</Text>
+          </Link>
+        </NextLink>
+      </Flex>
     </Container>
   </>
 )
